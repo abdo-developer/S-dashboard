77a525a6ce14e72c48660c35531c524e5603cb60
developer <developer@email.com>
2021-02-17 13:25:22 +0100

    Initial commit

---
+++ src/assets/js/script.js
@@ -0,0 +1,12 @@
+
+// Dyanamique Date Year Footer -- Copyright
+
+
+// jQuery(function () {
+
+//     // Copyright
+
+//     $("#copyright").text(" © All Rights Reserved For website | 2020 - " + new Date().getFullYear());
+
+// });
+
---
+++ src/index.js
@@ -0,0 +1,19 @@
+
+
+
+import './assets/sass/style.scss';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
+import 'jquery/dist/jquery';
+
+
+import 'popper.js/dist/popper';
+
+import '@fortawesome/fontawesome-free/js/all';
+
+import 'normalize.css/normalize.css';
+
+import './assets/js/script.js';
